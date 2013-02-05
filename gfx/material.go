843eab260a24840531f70f75aee4fe78c3efb9ce
Daniel Wiberg <wibbe@blueeye.se>
2013-02-05 22:54:20 +0100

    Started to implement shaders, and parameter sorting

--- gfx/material.go
+++ gfx/material.go
@@ -1,25 +1,32 @@
 package gfx
 
 type Parameter interface {
+	Priority() int
 	PreRender(ctx Context)
 	PostRender(ctx Context)
 }
 
+type parameters []Parameter
+
+func (p parameters) Len() int           { return len(p) }
+func (p parameters) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p parameters) Less(i, j int) bool { return p[i].Priority() > p[j].Priority() }
+
 type Material struct {
-	parameters map[string]Parameter
+	params parameters
 }
 
 func NewMaterial() *Material {
 	mat := Material{}
-	mat.parameters = make(map[string]Parameter)
+	//mat.params = make(map[string]Parameter)
 	return &mat
 }
 
 func (m *Material) AddParameter(name string, para Parameter) {
-	m.parameters[name] = para
+	//m.params[name] = para
 }
 
-// -- Shader --
+func (m *Material) Lookup(name string) Parameter {
 
-type Shader struct {
+	return nil
 }
---
+++ gfx/shader.go
@@ -0,0 +1,17 @@
+package gfx
+
+import (
+	gl "github.com/wibbe/glh/gl32c"
+)
+
+type Shader struct {
+	program gl.Int
+}
+
+func (s *Shader) PreRender(ctx *Context) {
+
+}
+
+func (s *Shader) PostRender(ctx *Context) {
+
+}
